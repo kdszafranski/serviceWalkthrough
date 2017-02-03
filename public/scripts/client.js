f5cbb64d0a32adaeb293e5f23a3b1fe55b86b062
Huck Brock <huckbrock@gmail.com>
2017-02-03 15:00:59 -0600

    adds random button and upsilo gif button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,20 +1,28 @@
 var app = angular.module('myApp', []);
 
-
-
 app.controller('GifController', function(GifGetter){
   console.log("connected?");
-
   var vm = this;
 
-GifGetter.randomGif().then(function(res){
-  console.log("from the controller", res);
+//search Query will be from text input
+  // vm.searchQuery = "UPsilon"
+vm.randomGif = function(){
+  GifGetter.randomGif().then(function(res){
+    console.log("from the controller", res);
+    vm.randomGifUrl = res.data.image_url;
+  });
+}
 
-  vm.randomGifUrl = res.data.image_url
 
-
-});
 // console.log("test:", GifGetter.randomGif());
+
+//searching gifs
+vm.searchGif = function(searchQuery){
+  GifGetter.searchGif(searchQuery).then(function(res){
+    //store array of gifs in searchGifUrl
+    vm.searchGifUrl = res.data;
+  })
+}
 
 
 
